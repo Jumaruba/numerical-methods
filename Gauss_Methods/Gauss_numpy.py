300430ed177470cf1ffb6bd3fb58a6f7c69b43a9
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2019-10-19 11:55:31 +0100

    Update Gauss_numpy.py

--- Gauss_Methods/Gauss_numpy.py
+++ Gauss_Methods/Gauss_numpy.py
@@ -1,16 +1,15 @@
 import numpy as np
 from scipy.linalg import lu
 
-np.set_printoptions(formatter={'float': lambda x: "{0:0.10f}".format(x)})   #format the number of decimal cases
+np.set_printoptions(formatter={'float': lambda x: "{0:0.10f}".format(x)})  # format the number of decimal cases
 
 matrix = np.array([[95., 3., 1., 2., 101.],
-          [4., 100., 10., 5., 119.],
-          [3., 9., 55., 5., 72.],
-          [10., 2., 4., 80., 96.]])
+                   [4., 100., 10., 5., 119.],
+                   [3., 9., 55., 5., 72.],
+                   [10., 2., 4., 80., 96.]])
 
+p, l, u = lu(matrix)  # transforming it into a triangular matrix
+for i in range(len(u)):  # diagonal to 1
+    u[i] -= (1 - 1 / u[i][i]) * u[i]
 
-pl, u = lu(matrix, permute_l=True)      #transforming it into a triangular matrix
-for i in range(len(u)):                 #diagonal to 1
-    u[i] -= (1-1/u[i][i])*u[i]
-    
 print(u)
