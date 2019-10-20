a3a9b835ba3d5ddc456b5081b30de8844bd8edd1
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2019-10-20 17:28:37 +0100

    Update newtonMethod_2variables.py

--- Real_Zeros_functions/Python/newtonMethod_2variables.py
+++ Real_Zeros_functions/Python/newtonMethod_2variables.py
@@ -19,15 +19,13 @@ def df1y(x,y):
 def df2y(x,y):
 	return -2*y
 
-def jacobian(x,y): 
-	return df1x(x,y)*df2y(x,y)-df1y(x,y)*df2x(x,y)
-
-def newtonMethod_2(y0, x0): 
-	x = x0
-	y = y0
+def newtonMethod_2(y, x): 
 	for i in range(20): 
-		x = x - (f1(x,y)*df2y(x,y)-df1y(x,y)*f2(x,y))/jacobian(x,y)
-		y = y - (df1x(x,y)*f2(x,y) - f1(x,y)*df2x(x,y))/jacobian(x,y)
-		print('n: {2}\t x: {0:.10f}\t y: {1:.10f}'.format(x,y,i))
+		jacobian = df1x(x,y)*df2y(x,y)-df1y(x,y)*df2x(x,y)
+		xn = x - (f1(x,y)*df2y(x,y)-df1y(x,y)*f2(x,y))/jacobian
+		yn = y - (df1x(x,y)*f2(x,y)-f1(x,y)*df2x(x,y))/jacobian 
+		x = xn
+		y = yn
+		print('n: {2}\t x: {0:.10f}\t y: {1:.10f}'.format(xn,yn,i))
 
 newtonMethod_2(1,2)
