4dad343fe82b48c9ed64fd054cdaf046657c40d5
jumaruba <juliane.marbayashi@gmail.com>
2019-11-09 16:27:31 +0000

    Gauss seidel

--- Gauss_Methods/Gauss_Seidel.py
+++ Gauss_Methods/Gauss_Seidel.py
@@ -12,7 +12,21 @@ matrix = [[3,1,1,7],
           [1,4,2,4],
           [0,2,5,5]]
 
-def gauss_Seidel(x1,x2,x3):     
+def gauss_Seidel(x1,x2,x3):  
+    x1_ant = x1
+    x2_ant = x2
+    x3_ant = x3
+    for i in range(20):    
+        x1 = _x1(x2,x3)
+        x2 = _x2(x1,x3)
+        x3 = _x3(x1,x2)
+        print("x1: %f\t x2: %f\t x3: %f\t" %(x1,x2,x3))
+        if (abs(x1-x1_ant) < 10**(-3) and abs(x2-x2_ant) < 10**(-3) and abs(x2-x2_ant) < 10**(-3)):     #checar o criterio de paragem
+            break
+        x1_ant = x1     
+        x2_ant = x2 
+        x3_ant = x3
 
+gauss_Seidel(0,0,0)
 
     
