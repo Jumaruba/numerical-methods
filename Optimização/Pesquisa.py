5c61d711ee5e418fe4ac77ae7a0d0070f0c3dbd7
jumaruba <juliane.marubayashi@gmail.com>
2019-12-06 14:36:01 +0000

    aurea done

--- Optimização/Pesquisa.py
+++ Optimização/Pesquisa.py
@@ -1,7 +1,7 @@
 import math as m
 
 def f(x):
-    return m.sin(x)
+    return (2*x+1)**2-5*m.cos(10*x)
 
 #estamos procurando pelo minimo local 
 def pesquisa(guess, step): 
@@ -19,3 +19,35 @@ def pesquisa(guess, step):
     return [x0,x1,x2]
 
 lista = pesquisa(2,0.1)
+
+
+def aurea_min(x1,x2, error):
+    b = (m.sqrt(5)-1)/2
+    a = b*b
+    while(abs(x1-x2) > error):
+        x3 = x1 + a*(x2-x1)  
+        x4 = x1 + b*(x2-x1)
+        if f(x3) < f(x4):
+            x1 = x1 
+            x2 = x4
+        else:
+            x1 = x3
+            x2 = x2 
+    return [x1,x2,x3,x4]
+
+def aurea_max(x1,x2, error):
+    b = (m.sqrt(5)-1)/2
+    a = b*b
+    while(abs(x1-x2) > error):
+        x3 = x1 + a*(x2-x1)  
+        x4 = x1 + b*(x2-x1)
+        if f(x3) > f(x4):
+            x1 = x1 
+            x2 = x4
+        else:
+            x1 = x3
+            x2 = x2 
+    return [x1,x2,x3,x4]
+
+print(aurea_min(-1,0,0.001))
+print(aurea_max(-1,0,0.001))
