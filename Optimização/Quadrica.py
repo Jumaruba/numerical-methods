5ce18738332871a7bafdecadae4e2a41b13d3edb
jumaruba <juliane.marubayashi@gmail.com>
2019-12-07 16:08:13 +0000

    golden and quadrica

---
+++ Optimização/Quadrica.py
@@ -0,0 +1,16 @@
+
+
+
+def dfx(x,y): 
+    return -2*y+4*x-4*sin(4*x)
+def dfy(x,y): 
+    return 2*y-2*x-6
+
+def quadrica(x,y):
+     
+    for i in range(20): 
+        xn = xn -     
+
+
+
+quadrica(1,1)
