e6e5320064fad37d99952eb8906202da63369548
jumaruba <juliane.marubayashi@gmail.com>
2019-12-13 14:44:39 +0000

    quadricas complete

--- Optimização/Quadrica.py
+++ Optimização/Quadrica.py
@@ -1,16 +1,30 @@
+import math as m 
 
-
-
+def f(x,y): 
+    return m.sin(y) + y*y/4 + m.cos(x) + x*x/4 -1 
 def dfx(x,y): 
-    return -2*y+4*x-4*sin(4*x)
+    return x/2 - m.sin(x)*y
 def dfy(x,y): 
-    return 2*y-2*x-6
+    return m.cos(y) + y/2 
+def dfxx(x,y): 
+    return 1/2 - m.cos(x)
+def dfyy(x,y):
+    return 1/2 - m.sin(y)
+def dfxy(x,y): 
+    return 0
+def dfyx(x,y): 
+    return 0
 
 def quadrica(x,y):
      
     for i in range(20): 
-        xn = xn -     
+        x_ant = x
+        y_ant = y 
+        det = dfxx(x_ant,y_ant) * dfyy(x_ant,y_ant) - dfxy(x_ant,y_ant) * dfyx(x_ant,y_ant) 
+        x = x_ant - (dfyy(x_ant,y_ant)*dfx(x_ant, y_ant) - dfxy(x_ant,y_ant)*dfy(x_ant,y_ant))/det
+        y = y_ant - (-dfxy(x_ant, y_ant)* dfx(x_ant, y_ant) + dfxx(x_ant, y_ant) * dfy(x_ant, y_ant))/det
+        print(x,y)
+    return [x,y]
 
-
-
-quadrica(1,1)
+r = quadrica(0,0)
+print(f(r[0],r[1]))
