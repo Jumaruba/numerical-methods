103352a2b0932e2076e7bbdd1fd023a026580582
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2020-01-10 16:06:10 +0000

    Update Pesquisa.py

--- Optimização/Pesquisa.py
+++ Optimização/Pesquisa.py
@@ -27,30 +27,34 @@ lista = pesquisa(2, 0.1)
 def aurea_min(x1, x2, error):
     b = (m.sqrt(5) - 1) / 2
     a = b * b
+    x3 = x1 + a * (x2 - x1)
+    x4 = x1 + b * (x2 - x1)
     while abs(x1 - x2) > error:
-        x3 = x1 + a * (x2 - x1)
-        x4 = x1 + b * (x2 - x1)
         if f(x3) < f(x4):
             x1 = x1
             x2 = x4
+            x3 = x1 + a * (x2 - x1)
         else:
             x1 = x3
             x2 = x2
+            x4 = x1 + b * (x2 - x1)
     return [x1, x2, x3, x4]
 
 
 def aurea_max(x1, x2, error):
     b = (m.sqrt(5) - 1) / 2
     a = b * b
+    x3 = x1 + a * (x2 - x1)
+    x4 = x1 + b * (x2 - x1)
     while abs(x1 - x2) > error:
-        x3 = x1 + a * (x2 - x1)
-        x4 = x1 + b * (x2 - x1)
         if f(x3) > f(x4):
             x1 = x1
             x2 = x4
+            x3 = x1 + a * (x2 - x1)
         else:
             x1 = x3
             x2 = x2
+            x4 = x1 + b * (x2 - x1)
     return [x1, x2, x3, x4]
 
 
