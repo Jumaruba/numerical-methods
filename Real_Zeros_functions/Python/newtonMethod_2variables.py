47a74fc1155015cc3f08ce50f3f8dddf2eb4b41e
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2020-01-12 17:36:04 +0000

    Update newtonMethod_2variables.py

--- Real_Zeros_functions/Python/newtonMethod_2variables.py
+++ Real_Zeros_functions/Python/newtonMethod_2variables.py
@@ -34,7 +34,7 @@ def newtonMethod_2(y0, x0):
     y = y0
     for i in range(20):
         x = x - (f1(x, y) * df2y(x, y) - df1y(x, y) * f2(x, y)) / jacobian(x, y)
-        y = y - (df1x(x, y) * f2(x, y) - f2(x, y) * df1x(x, y)) / jacobian(x, y)
+        y = y - (df1x(x, y) * f2(x, y) - df2x(x, y) * f1(x, y)) / jacobian(x, y)
         print('n: {2}\t x: {0:.10f}\t y: {1:.10f}'.format(x, y, i))
 
 
